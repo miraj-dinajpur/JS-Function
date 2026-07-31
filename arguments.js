baaa244925b34071e9db9f0ba2d61aa53c81ae51
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-08-01 01:25:14 +0600

    arguments.js added

---
+++ arguments.js
@@ -0,0 +1,23 @@
+// arguments 
+
+function multiply(a, b) { // parameters
+    const result = a * b;
+    console.log('result of multiplication is:', result);
+}
+
+// multiply(3, 5, 7, 98, 71); // arguments
+
+
+function square(number) {
+    const result = number * number;
+    console.log('square of the number provided', result)
+}
+
+// console.log(' number ase naki bhai', number)
+// console.log(' number ase naki bhai', result)
+
+
+square(7);
+console.log('--------------------')
+const money = 25;
+square(money);
