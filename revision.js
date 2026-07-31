ab7781ed68714eb28a1a8af30303b6ddde3631b9
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-08-01 01:19:37 +0600

    revision.js added

---
+++ revision.js
@@ -0,0 +1,18 @@
+/**
+ * Variable
+ * conditionals
+ * loop
+ * array
+ * objects
+ * 
+*/
+
+const price = 25;
+if (price > 20) {
+    console.log('I will buy it')
+}
+for (let i = 0; i < 10; i++) {
+
+}
+const numbers = [2, 3, 4, 6, 6]
+
